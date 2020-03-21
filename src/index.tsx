7fa90c1afacb330ef7fec88d3da2938ed87d093d
Cisco D <46197605+cisco2d@users.noreply.github.com>
2020-03-21 03:47:59 -0700

    example todo app finished

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+// types used to reference other types
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+// interface used to create new type
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+// You can extend interfaces
+// interface ITodo2 extends ITodo {
+//   tags: string[]
+// }
+
 export default function App(): JSX.Element {
-  const sum = (a: number, b: number): number => a + b;
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text: value, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos]; //can't do = todos;
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    // [0,1,2,3].splice(2,1), returns [3], array = [0,1,2]
+    newTodos.splice(index, 1)
+    setTodos(newTodos);
+  }
 
   return (
-    <h1>My Favorite Number is {sum(3,8)}</h1>
-  )
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <section>
+        {todos.map((todo: ITodo, index: number) => {
+          return (
+            <Fragment key={index}>
+              <div style={{textDecoration: todo.complete ? 'line-through': ''}}>{todo.text}</div>
+              <button type="button" onClick={() => completeTodo(index)}>
+                { !todo.complete ? 'Incomplete' : 'Complete'}
+              </button>
+              <button type='button' onClick={() => removeTodo(index)}>X</button>
+            </Fragment>
+          );
+        })}
+      </section>
+    </Fragment>
+  );
 }
 
 const root = document.getElementById('app-root');
